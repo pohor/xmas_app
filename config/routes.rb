161e400b371d3104b00cc9564b915f1d35cb4cc7
Ola Mrozek <aleksandra.pohorecka@chargify.com>
2020-05-11 12:59:02 +0200

    Add root page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
 
-  root to: "users#show"
+  root to: "home#home"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
