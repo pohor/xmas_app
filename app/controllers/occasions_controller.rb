295b4ed48dac6440cff70a835d2f0e1b1407b92a
Ola Mrozek <aleksandra.pohorecka@chargify.com>
2020-05-19 19:19:45 +0200

    Add create form + show view for occasion

--- app/controllers/occasions_controller.rb
+++ app/controllers/occasions_controller.rb
@@ -1,4 +1,27 @@
 class OccasionsController < ApplicationController
  def new
+   @occasion = Occasion.new
  end
+
+  def create
+    @occasion = Occasion.new(occasion_params)
+    if @occasion.save
+      flash[:message] = "You've added an occasion!"
+      redirect_to @occasion
+    else
+      @errors = @occasion.errors
+      flash[:error] = @errors
+      render :new
+    end
+  end
+
+ def show
+   @occasion = Occasion.find(params[:id])
+ end
+
+  private
+
+  def occasion_params
+    params.require(:occasion).permit(:name, :date)
+  end
 end
