e046b91c5a00b618cf10d6aa4557bdc59978e8ea
Ola Mrozek <aleksandra.pohorecka@chargify.com>
2020-05-25 11:51:44 +0200

    Add gifters actions

---
+++ app/controllers/gifters_controller.rb
@@ -0,0 +1,32 @@
+class GiftersController < ApplicationController
+
+  def show
+    @gifter = Gifter.find(params[:id])
+    @occasion = @gifter.occasion
+  end
+
+  def new
+    @occasion = Occasion.find(params[:occasion_id])
+    @gifter = @occasion.gifters.build
+  end
+
+  def create
+    @occasion = Occasion.find(params[:occasion_id])
+    @gifter = @occasion.gifters.new(gifter_params)
+    @gifter.user = current_user
+
+    if @gifter.save
+      flash[:notice] = "You have successfuly added yourself as a participant!"
+      redirect_to [@occasion, @gifter]
+    else
+      flash[:error] = @gifter.errors
+      render :new
+    end
+  end
+
+  private
+
+  def gifter_params
+    params.require(:gifter).permit(:name, :user_id, :occasion_id, :present_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
 
   root to: "home#home"
 
-  resources :occasions
+  resources :occasions do
+    resources :gifters
+  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
