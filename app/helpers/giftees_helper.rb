a3538c73fda7447af44d16f816ee1ffcec91fe70
Ola Pohorecka Mrożek <aleksandra.pohorecka@chargify.com>
2020-07-19 11:23:43 +0200

    Refactor

--- app/helpers/giftees_helper.rb
+++ app/helpers/giftees_helper.rb
@@ -2,4 +2,8 @@ module GifteesHelper
   def has_gifter?(giftee)
     giftee.gifter.present?
   end
+
+  def presents_reserved_by_gifter_for_giftee(giftee)
+    giftee.presents.where(gifter_id: gifter.id)
+  end
 end
