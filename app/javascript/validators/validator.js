ab72eeec3138135a5f9ddd23450470691b430d1e
Ola Pohorecka Mrożek <aleksandra.pohorecka@chargify.com>
2020-07-28 21:07:37 +0200

    Enhance validation

--- app/javascript/validators/validator.js
+++ app/javascript/validators/validator.js
@@ -29,37 +29,61 @@ const enableButtons = (buttons) => {
     )
 };
 
+const checkForEmptyFields = (fields) => {
+    return fields.some(element => element.value === (''))
+};
+
+const disableSubmitOnEmptyField = (fields, buttons) => {
+    if ( checkForEmptyFields(fields)) {
+        disableButtons(buttons)
+    } else {
+        enableButtons(buttons)
+    }
+};
+
 document.addEventListener("turbolinks:load", function() {
     if (document.getElementsByClassName('validate_this_field')) {
-        const fieldsToValidate = document.getElementsByClassName('validate_this_field')
-        Array.from(fieldsToValidate).forEach(field =>
-            field.addEventListener('blur', (event) => {
-                const buttons = Array.from(document.getElementsByClassName('button'));
-                const fieldValue = field.value;
-                const fieldName = field.name;
-                const controllerName = document.getElementById('controller_name').value
-                const helpText = document.getElementById(`${fieldName.split('[')[1].replace(']','')}_help_text`);
-                clearHelpText(helpText, field);
-                validateInput(`/${controllerName}/validate` + `?` + `${fieldName}` + `=` + `${fieldValue || ""}` )
-                    .then(data => {
-                            if (data.valid) {
-                                field.classList.add('is-success');
-                                enableButtons(buttons);
-                            } else {
-                                event.preventDefault();
-                                disableButtons(buttons);
-                                field.classList.add('is-danger');
-                                helpText.style.removeProperty('display');
-                                helpText.classList.add('help');
-                                helpText.classList.add('is-danger');
-                                const text = document.createTextNode(`${data.message[0]}`);
-                                helpText.appendChild(text);
-                            }
+
+        const fieldsToValidate = Array.from(document.getElementsByClassName('validate_this_field'))
+        const buttons = Array.from(document.getElementsByClassName('button'));
+        let timeout = null;
+
+        disableSubmitOnEmptyField(fieldsToValidate, buttons);
+
+        fieldsToValidate.forEach(field =>
+            ['keyup', 'change', 'click'].forEach(function (e) {
+                    field.addEventListener(e, function () {
+                            clearTimeout(timeout);
+
+                            const fieldValue = field.value;
+                            const fieldName = field.name;
+                            const controllerName = document.getElementById('controller_name').value
+                            const helpText = document.getElementById(`${fieldName.split('[')[1].replace(']', '')}_help_text`);
+
+                            clearHelpText(helpText, field);
+
+                            timeout = setTimeout(function () {
+                                validateInput(`/${controllerName}/validate` + `?` + `${fieldName}` + `=` + `${fieldValue || ""}`)
+                                    .then(data => {
+                                            if (data.valid) {
+                                                field.classList.add('is-success');
+                                                enableButtons(buttons);
+                                                disableSubmitOnEmptyField(fieldsToValidate, buttons);
+                                            } else {
+                                                disableButtons(buttons);
+                                                field.classList.add('is-danger');
+                                                helpText.style.removeProperty('display');
+                                                helpText.classList.add('help');
+                                                helpText.classList.add('is-danger');
+                                                const text = document.createTextNode(`${data.message[0]}`);
+                                                helpText.appendChild(text);
+                                            }
+                                        }
+                                    )
+                            }, 250)
                         }
                     )
-            })
-        )};
-})
-
-
-
+                }
+            )
+        )}
+    })
